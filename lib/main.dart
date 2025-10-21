de7c808c4810ae8501bc857b567ae7075ba3e1a2
EyaBenSlamaaa <eya.benslama.eya@gmail.com>
2025-10-21 23:36:33 +0100

    partie2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/home_screen.dart';
 import 'screens/notes_screen.dart';
 
-void main() {
-  runApp(NotesApp());
+Future<void> main() async {
+  // S'assurer que Flutter est initialisé
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Charger les variables d’environnement depuis le fichier .env
+  await dotenv.load(fileName: ".env");
+
+  // Lancer l’application
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
